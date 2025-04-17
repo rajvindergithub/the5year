0d1fc44b809ab494871d9dbfc15de443c10188f3
rajvindergithub <rajvindergithub@gmail.com>
2025-04-17 20:54:38 +0530

    html structure

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Example Title</title>
+    <meta name="author" content="Your Name">
+    <meta name="description" content="Example description">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link rel="icon" type="image/x-icon" href="" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css"  />
+
+</head>
+
+<body>
+
+
+    <header></header>
+    <main>
+
+
+        <section id="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="banner-h-s">
+                            <div class="b-heading">
+                                <h1>Turn your trading passion into your trading career</h1>
+                            </div>
+                            <div class="b-h-cta">
+                                <a href="">Start Trading</a>
+                            </div>
+                        </div>
+                        <div class="banner-trust-prop">
+                            <ul>
+                                <li><img src="assets/images/trust_pil.png" /></li>
+                                <li><img src="assets/images/divider.png" /></li>
+                                <li><img src="assets/images/propfirmmatch.png" /></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-5"></div>
+                </div>
+            </div>
+        </section>
+        
+        <section id="elite-trader">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="e-t-heading">
+                            <h2>
+                                You’ve got what it takes to<br class="hide-md" />become an Elite Trader. 
+                            </h2>
+                        </div>
+                        <div class="e-t-s-h">
+                            We’re here to make you one. 
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="et-block">
+                            <div class="et-b-img">
+                                <img src="assets/images/elite-trader/fair-play-first.png" />
+                            </div>
+                            <div class="et-b-h"><h3>Fair play<br class="hide-md" />first</h3></div>
+                            <div class="et-b-s-h">
+                                <p>All programs scale. Zero-fuss<br class="hide-md" />payouts with no questions asked.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                     <div class="col-md-4">
+                        <div class="et-block">
+                            <div class="et-b-img"> <img src="assets/images/elite-trader/trade-with-confidence.png" /></div>
+                            <div class="et-b-h"><h3>Trade with<br class="hide-md" />confidence</h3></div>
+                            <div class="et-b-s-h">
+                                <p>Up to $4M in funded trading with<br class="hide-md" />industry-beating spreads</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                     <div class="col-md-4">
+                        <div class="et-block">
+                            <div class="et-b-img">
+                                 <img src="assets/images/elite-trader/expert.png" />
+                            </div>
+                            <div class="et-b-h"><h3>Expert guidance,<br class="hide-md" />24/7 support</h3></div>
+                            <div class="et-b-s-h">
+                                <p>We’re here to make you a better<br class="hide-md" />trader, day in and day out.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                     
+                    
+                </div>
+            </div>
+        </section>
+        
+        <section id="trader-stories">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="t-s-h-s">
+                            Trader stories
+                        </div>
+                        <div class="t-s-h">
+                            <h2>The5ers is the home of <br class="hide-md" />funded trading</h2>
+                        </div>
+                        <div class="t-s-h-s-p">
+                            <p>
+                                Since 2016, we’ve helped traders bring their strategies to life with the industry’s best funded trading programs. Welcome home.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+         <section id="funded-trading-programs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="ftp-h">
+                            Funded trading programs
+                        </div>
+                        <div class="ftp-h-s">
+                            <h2>Design your trading journey</h2>
+                        </div>
+                        <div class="ftp-h-s-p">
+                            <p>
+                               Choose the best program for your skill level and risk appetite.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section id="program-advantages">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pa-h">
+                            Program advantages
+                        </div>
+                        <div class="pa-h-s">
+                            <h2>Everything you need to succeed <br class="hide-md" /> (and a whole lot more)</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pa-block-main">
+                            <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">Instant, simple payouts</div>
+                            </div>
+                             <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">All trading  systems welcome</div>
+                            </div>
+                             <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">Fastest growth plans</div>
+                            </div>
+                             <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">24/7 email and phone support </div>
+                            </div>
+                             <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">Transparent one-time fees</div>
+                            </div>
+                             <div class="pa-b">
+                                <div class="pa-b-img"></div>
+                                <div class="pa-b-heading">Free educational resources</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+         <section id="funded-trading-programs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="ftp-h">
+                            Funded trading programs 
+                        </div>
+                        <div class="ftp-h-s">
+                            <h2>Design your trading journey</h2>
+                        </div>
+                        <div class="ftp-h-s-p">
+                            <p>
+                               Choose the best program for your skill level and risk appetite.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <footer></footer>
+
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
